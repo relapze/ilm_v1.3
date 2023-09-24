38f69b4b01ff4099e24d6ce1bc3679a33a020a38
Lumojino <lumojino@gmail.com>
2023-09-24 21:49:45 +0200

    jsj

--- vite.config.ts
+++ vite.config.ts
@@ -25,8 +25,14 @@ export default defineConfig({
       },
     },
   },
-
   build: {
-    target: ["esnext"],
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+    onwarn(warning, warn) {
+      if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+        return
+      }
+      warn(warning)
+    }}
   },
 });
