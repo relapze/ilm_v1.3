d6bb97c2596967ee53dd8244d58bcc638af12feb
Lumojino <lumojino@gmail.com>
2023-09-25 16:13:27 +0200

    jdisjfis

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,39 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
+ 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   define: {
     global: (() => {
-      let globalVariable = 'globalThis';
+      if (command !== "build") return "globalThis";
+      let globalVariable = "globalThis";
       try {
         // Try to import @safe-global/safe-apps-provider
-        require.resolve('@safe-global/safe-apps-provider');
+        require.resolve("@safe-global/safe-apps-provider");
         // Try to import @safe-global/safe-apps-sdk
-        require.resolve('@safe-global/safe-apps-sdk');
-        // If both modules are found, return the custom global variable
-        globalVariable = 'global';
-       } catch (e) {
+        require.resolve("@safe-global/safe-apps-sdk");
+        // If both modules are found, return custom global variable
+        globalVariable = "global";
+      } catch (e) {
         // If either module is not found, fallback to globalThis
-        globalVariable = 'globalThis';
-       }
+        globalVariable = "globalThis";
+      }
       return globalVariable;
-     })()
-   },
+    })(),
+  },
+ 
   resolve: {
     alias: {
       process: "process/browser",
       util: "util",
     },
   },
+ 
   plugins: [react()],
-
+ 
   optimizeDeps: {
     esbuildOptions: {
-      target: "es2020",
+      target: "esnext",
       define: {
         global: "globalThis",
       },
@@ -39,15 +42,8 @@ export default defineConfig({
       },
     },
   },
+ 
   build: {
-    target: "es2020",
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-    onwarn(warning, warn) {
-      if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-        return
-      }
-      warn(warning)
-    }}
+    target: ["esnext"],
   },
-});
+}));
